7f7a1d674c3f1dd298aea4eb9e609384ace528ea
Lenivaya <xocada@gmail.com>
2019-10-27 21:03:13 +0200

    xmonad?
    Former-commit-id: 0f589209ff16116440973100f762b706094a8412

--- xmonad/.xmonad/src/XMonad/Custom/Layout.hs
+++ xmonad/.xmonad/src/XMonad/Custom/Layout.hs
@@ -26,6 +26,16 @@ import           XMonad.Layout.SubLayouts
 import           XMonad.Layout.Tabbed
 import           XMonad.Layout.WindowNavigation
 import           XMonad.Layout.ResizableTile
+import           XMonad.Layout.Gaps
+import           XMonad.Layout.IfMax
+import           XMonad.Layout.ThreeColumns
+import           XMonad.Layout.NoBorders
+import           XMonad.Layout.Named
+import           XMonad.Layout.ResizableTile
+import           XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import           XMonad.Layout.Renamed (renamed, Rename(CutWordsLeft, Replace))
+import           XMonad.Layout.OneBig
+
 
 applySpacing :: l a -> ModifiedLayout Spacing l a
 applySpacing = spacingRaw False (Border 6 6 6 6) True (Border 6 6 6 6) True
@@ -36,17 +46,80 @@ data CustomTransformers = GAPS
 instance Transformer CustomTransformers Window where
     transform GAPS x k = k (avoidStruts $ applySpacing x) (const x)
 
-layoutHook = fullscreenFloat
-             . smartBorders
-             $ lessBorders OnlyLayoutFloat
-             $ mkToggle (single NBFULL)
+data Gaps' = Gaps'
+  { u  :: Int
+  , d  :: Int
+  , x  :: Int
+  , x' :: Integer
+  }
+
+gs :: Gaps'
+gs = Gaps'
+  { u  = 30
+  , d  = 20
+  , x  = 15
+  , x' = 15
+  }
+
+gapses :: l a -> ModifiedLayout Gaps l a
+gapses     = gaps [(U, u gs), (R, x gs), (L, x gs), (D, d gs)]
+
+spacingses :: l a -> ModifiedLayout Spacing l a
+spacingses = spacingRaw True (Border      0  (x' gs) (x' gs) (x' gs))
+                        True (Border (x' gs) (x' gs) (x' gs) (x' gs))
+                        True
+
+space = renamed [Replace "space"] 
+        $ limitWindows 4  
+        $ spacing 12 
+        $ Mirror 
+        $ mkToggle (single MIRROR) 
+        $ mkToggle (single REFLECTX) 
+        $ mkToggle (single REFLECTY) 
+        $ OneBig (2/3) (2/3)
+
+oneBig = renamed [Replace "oneBig"]
+        $ limitWindows 6  
+        $ Mirror 
+        $ mkToggle (single MIRROR) 
+        $ mkToggle (single REFLECTX) 
+        $ mkToggle (single REFLECTY) 
+        $ OneBig (5/9) (8/12)
+
+full = named "Fullscreen"
+     $ noBorders (fullscreenFull Full)
+tall = named "Tall"
+     $ IfMax 1 full
+     $ gapses
+     . spacingses
+     $ ResizableTall 1 (2/100) (1/2) []
+
+tcm  = named "Three Columns"
+     $ IfMax 1 full
+     $ gapses
+     $ hiddenWindows
+     . spacingses
+     $ ThreeColMid 1 (1/10) (1/2)
+
+bsp = named "BSP"
              $ avoidStruts
              $ applySpacing
-          -- $ mkToggle (single GAPS)
-             $ mkToggle (single REFLECTX)
-             $ mkToggle (single REFLECTY)
+             $ lessBorders OnlyLayoutFloat
              $ windowNavigation
-             $ addTabs shrinkText tabTheme
              $ hiddenWindows
+             $ addTabs shrinkText tabTheme
+          -- $ mkToggle (single GAPS)
              $ subLayout [] (Simplest ||| Accordion)
              emptyBSP
+
+layoutHook = fullscreenFloat
+             . smartBorders
+             $ mkToggle (single REFLECTX)
+             $ mkToggle (single REFLECTY)
+             $ mkToggle (single NBFULL)
+
+             $ bsp 
+          ||| tcm
+          ||| tall
+          ||| oneBig
+          ||| space
