b09465169b11207a737880f742c69bec5705b57c
lenivaya <xocada@gmail.com>
2019-11-30 15:10:56 +0200

    some changes
    Former-commit-id: c62de5821c94a1bae7cebc5ba095d3d41e297f71

--- xmonad/.xmonad/src/XMonad/Custom/Event.hs
+++ xmonad/.xmonad/src/XMonad/Custom/Event.hs
@@ -8,7 +8,7 @@ import           XMonad.Custom.Scratchpads
 import           XMonad.Hooks.EwmhDesktops
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Util.Loggers.NamedScratchpad
-import XMonad.Hooks.RefocusLast
+import           XMonad.Hooks.RefocusLast
 import           qualified Data.Map.Strict as M
 
 -- Keeps last focused window
