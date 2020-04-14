9e7b338d039073ef09811d492f07d4af38b431d8
Lenivaya <xocada@gmail.com>
2020-04-14 22:13:01 +0300

    Formatted with prettier
    Former-commit-id: e3c0995070369fe368097caffbce2a25c29032e0

--- config/xmonad/src/XMonad/Actions/PerConditionKeys.hs
+++ config/xmonad/src/XMonad/Actions/PerConditionKeys.hs
@@ -1,23 +1,26 @@
 module XMonad.Actions.PerConditionKeys
-    ( XCond(..)
-    , chooseAction
-    , bindOn
-    ) where
+  ( XCond(..)
+  , chooseAction
+  , bindOn
+  )
+where
 
 import           Data.List
 import           XMonad
-import qualified XMonad.StackSet as S
+import qualified XMonad.StackSet               as S
 
 data XCond = WS | LD
 
 chooseAction :: XCond -> (String -> X ()) -> X ()
 chooseAction WS f = withWindowSet (f . S.currentTag)
-chooseAction LD f = withWindowSet (f . description . S.layout . S.workspace . S.current)
+chooseAction LD f =
+  withWindowSet (f . description . S.layout . S.workspace . S.current)
 
 bindOn :: XCond -> [(String, X ())] -> X ()
 bindOn xc bindings = chooseAction xc chooser
-    where chooser x = case find ((x ==) . fst) bindings of
-                           Just (_, action) -> action
-                           Nothing          -> case find (("" ==) . fst) bindings of
-                                                    Just (_, action) -> action
-                                                    Nothing          -> return ()
+ where
+  chooser x = case find ((x ==) . fst) bindings of
+    Just (_, action) -> action
+    Nothing          -> case find (("" ==) . fst) bindings of
+      Just (_, action) -> action
+      Nothing          -> return ()
