2e8f316b254a84252dc55e185359c2b12c76061f
Lenivaya <xocada@gmail.com>
2020-04-14 22:11:18 +0300

    Seems too complicated
    Former-commit-id: 54c084e2e4e813c8f1bbf2747d90fecb8b219666

--- modules/develop/go.nix
+++ modules/develop/go.nix
@@ -8,23 +8,6 @@
     goPath = "go";
   };
 
-  # For writting in emacs
-  my.home.programs.go.packages = {
-    "github.com/motemen/gore/cmd/gore" =
-      builtins.fetchGit "https://github.com/motemen/gore";
-    "github.com/mdempsky/gocode" =
-      builtins.fetchGit "https://go.googlesource.com/tools";
-    "golang.org/x/tools/cmd/godoc" =
-      builtins.fetchGit "https://go.googlesource.com/tools";
-    "golang.org/x/tools/cmd/goimports" =
-      builtins.fetchGit "https://go.googlesource.com/tools";
-    "golang.org/x/tools/cmd/gorename" =
-      builtins.fetchGit "https://go.googlesource.com/tools";
-    "golang.org/x/tools/cmd/guru" =
-      builtins.fetchGit "https://go.googlesource.com/tools";
-    "github.com/cweill/gotests/..." =
-      builtins.fetchGit "https://github.com/cweill/gotests";
-    "github.com/fatih/gomodifytags" =
-      builtins.fetchGit "https://github.com/fatih/gomodifytags";
-  };
+  my.packages = with pkgs.unstable; [ goimports ];
+
 }
