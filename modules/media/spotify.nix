498868f39735bc7dead00d2d5000815ead8f6afc
Lenivaya <xocada@gmail.com>
2020-04-16 21:47:12 +0300

    Wow, i don't need that
    Former-commit-id: d29535943d5b59501902ee613885e3d535b9e658

--- modules/media/spotify.nix
+++ modules/media/spotify.nix
@@ -1,5 +1,3 @@
-# I use spotify for my music needs. Gone are the days where I'd manage 200gb+ of
-# local music; most of which I haven't heard or don't even like.
 
 { config, options, lib, pkgs, ... }:
 with lib; {
@@ -11,11 +9,6 @@ with lib; {
   };
 
   config = mkIf config.modules.media.spotify.enable {
-    # services.spotifyd doesn't work so we'll have to roll our own spotifyd +
-    # spotify-tui solution. The dbus interface doesn't work, though, so we have to
-    # wait for spotify-tui to implement CLI command for controlling spotify
-    # playback. See https://github.com/Rigellute/spotify-tui/issues/147 or for
-    # spotifyd to get fixed.
     my.packages = with pkgs;
       [
         # unstable.spotifyd
@@ -28,9 +21,6 @@ with lib; {
         #   exec ${unstable.spotify-tui}/bin/spt
         # '')
 
-        # spotify-tui is fine for selecting and playing music, but incomplete. We
-        # occasionally still need the official client for more sophisticated search
-        # and the "made for you" playlists.
         spotify
 
         # Since dbus doesn't work with spotifyd/spotify-tui, and I always use tmux
