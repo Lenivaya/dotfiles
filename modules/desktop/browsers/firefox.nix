a3d060727ac77670fe3b88cc0b6775d550c9e5d9
Lenivaya <xocada@gmail.com>
2020-04-26 21:42:17 +0300

    Add firefox profile and enable hardware acceleration
    Former-commit-id: 245b9e0bfc82d18f0dd0dafbacb0d4e15ab017dc

--- modules/desktop/browsers/firefox.nix
+++ modules/desktop/browsers/firefox.nix
@@ -1,30 +1,58 @@
 { config, options, lib, pkgs, ... }:
+
 with lib; {
   options.modules.desktop.browsers.firefox = {
     enable = mkOption {
       type = types.bool;
       default = false;
     };
+    profileName = mkOption {
+      type = types.str;
+      default = config.my.username;
+    };
   };
 
   config = mkIf config.modules.desktop.browsers.firefox.enable {
 
-    my.packages = with pkgs; [
-      firefox-bin
-      (pkgs.writeScriptBin "firefox-private" ''
-        #!${stdenv.shell}
-        ${firefox}/bin/firefox --private-window "$@"
-      '')
-      (makeDesktopItem {
-        name = "firefox-private";
-        desktopName = "Firefox (Private)";
-        genericName = "Open a private Firefox window";
-        icon = "firefox";
-        exec = "${firefox-bin}/bin/firefox --private-window";
-        categories = "Network";
-      })
-    ];
+    my.home.programs.firefox = {
+      enable = true;
+      package = pkgs.firefox;
+    };
+
+    my.packages = with pkgs;
+      [
+        (makeDesktopItem {
+          name = "firefox-private";
+          desktopName = "Firefox (Private)";
+          genericName = "Open a private Firefox window";
+          icon = "firefox";
+          exec = "${firefox}/bin/firefox --private-window";
+          categories = "Network";
+        })
+      ];
 
     my.env.XDG_DESKTOP_DIR = "$HOME"; # prevent firefox creating ~/Desktop
+
+    my.home.home.file = let cfg = config.modules.desktop.browsers.firefox;
+    in {
+      ".mozilla/firefox/profiles.ini".text = ''
+        [Profile0]
+        Name=default
+        IsRelative=1
+        Path=${cfg.profileName}.default
+        Default=1
+        [General]
+        StartWithLastProfile=1
+        Version=2
+      '';
+    };
+
+    # Hardware-acceleration
+    my.home.programs.firefox.profiles.default.settings = {
+      "layers.acceleration.force-enabled" = true;
+      "layers.omtp.enabled" = true;
+      "layout.display-list.retain" = true;
+      "layout.display-list.retain.chrome" = true;
+    };
   };
 }
