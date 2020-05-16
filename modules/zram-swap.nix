3a9e63b33f3d055e41603024109580c6b26580d8
Lenivaya <xocada@gmail.com>
2020-05-16 10:41:46 +0300

    Zram as module
    Former-commit-id: 37c63d592d62804a4d5e2d2fd840dc18600dd183

--- modules/zram-swap.nix
+++ modules/zram-swap.nix
@@ -1,9 +1,21 @@
-{
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 25;
-    numDevices = 1;
-    priority = 1;
+{ config, options, lib, ... }:
+with lib;
+let cfg = config.modules.zram;
+in {
+  options.modules.zram = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    zramSwap = {
+      enable = true;
+      algorithm = "zstd";
+      memoryPercent = 25;
+      numDevices = 1;
+      priority = 1;
+    };
   };
 }
