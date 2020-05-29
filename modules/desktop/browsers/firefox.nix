0ee4f8ee8761d99ca4f85b87654406349dd66d8b
Lenivaya <xocada@gmail.com>
2020-05-29 17:37:14 +0300

    Add userChrome.css to Firefox module
    Former-commit-id: 507de7f96a2a83633e9e9d2a10474ac0480ae70a

--- modules/desktop/browsers/firefox.nix
+++ modules/desktop/browsers/firefox.nix
@@ -47,16 +47,61 @@ with lib; {
       '';
     };
 
-    my.home.programs.firefox.profiles.default.settings = {
-      "browser.tabs.closeWindowWithLastTab" = false;
-      "browser.tabs.insertAfterCurrent" = true;
-      "browser.tabs.loadBookmarksInTabs" = true;
+    my.home.programs.firefox.profiles.default = {
+      settings = {
+        "browser.tabs.closeWindowWithLastTab" = false;
+        "browser.tabs.insertAfterCurrent" = true;
+        "browser.tabs.loadBookmarksInTabs" = true;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
-      # Hardware-acceleration
-      "layers.acceleration.force-enabled" = true;
-      "layers.omtp.enabled" = true;
-      "layout.display-list.retain" = true;
-      "layout.display-list.retain.chrome" = true;
+        # Hardware-acceleration
+        "layers.acceleration.force-enabled" = true;
+        "layers.omtp.enabled" = true;
+        "layout.display-list.retain" = true;
+        "layout.display-list.retain.chrome" = true;
+      };
+
+      userChrome = ''
+        /* @namespace url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"); */
+
+        @-moz-document url(chrome://browser/content/browser.xul),
+                        url(chrome://browser/content/browser.xhtml)
+        {
+          /* Remove the colored overline on the focused tab */
+          .tabbrowser-tab .tab-line { background: none !important; }
+
+          /* Replace favicon on tabs with close button on mouse hover */
+          .tabbrowser-tab:not(:hover) .tab-close-button,
+          .tabbrowser-tab:not([pinned]):hover .tab-icon-image { display: none !important; }
+          .tabbrowser-tab:not([pinned]):hover .tab-close-button { display:block !important; }
+
+          .tabbrowser-tab:hover .tab-throbber,
+          .tabbrowser-tab:hover .tab-icon-image,
+          .tabbrowser-tab:hover .tab-sharing-icon-overlay,
+          .tabbrowser-tab:hover .tab-icon-overlay,
+          .tabbrowser-tab:hover .tab-label-container,
+          .tabbrowser-tab:hover .tab-icon-sound {
+            -moz-box-ordinal-group: 2 !important;
+          }
+
+          .tabbrowser-tab .tab-close-button {
+            margin-left: -2px !important;
+            margin-right: 4px !important;
+          }
+
+          .tab-close-button:hover {
+            fill-opacity: 0 !important;
+          }
+
+          .tabbrowser-tab::after,
+          .tabbrowser-tab::before {
+            border-left: none !important;
+            border-right: none !important;
+          }
+
+          .scrollbutton-up[orient="horizontal"]~spacer { border-width: 0px; opacity: 0 }
+        }
+      '';
     };
 
   };
