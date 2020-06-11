f8159088233bde36aa6e76d493ad5ac7b94d486f
Lenivaya <xocada@gmail.com>
2020-06-11 17:24:00 +0300

    Wrote a wrapper which runs chromium with CLI flags from config
    Former-commit-id: 0b9588f92cc4fc09139ec01f60c7fd7128169809

--- modules/desktop/browsers/chromium.nix
+++ modules/desktop/browsers/chromium.nix
@@ -1,15 +1,28 @@
 { config, options, lib, pkgs, ... }:
 
-let cfg = config.modules.desktop.browsers.chromium;
+let
+  cfg = config.modules.desktop.browsers.chromium;
+
+  wrapWithFlags = p: f:
+    with pkgs;
+    writeScriptBin "${p.packageName}" ''
+      #!${stdenv.shell}
+      exec ${p}/bin/${p.packageName} ${lib.concatStringsSep " " f}
+    '';
+
 in with lib; {
   options.modules.desktop.browsers.chromium = {
     enable = mkOption {
       type = types.bool;
       default = false;
     };
-    ungoogled = mkOptions {
+    ungoogled = mkOption {
       type = types.bool;
       default = false;
+    };
+    flags = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
     };
   };
 
@@ -18,20 +31,32 @@ in with lib; {
       enable = true;
 
       package = if cfg.ungoogled then
-        pkgs.unstable.ungoogled-chromium
+        (wrapWithFlags pkgs.unstable.ungoogled-chromium cfg.flags)
       else
-        pkgs.chromium;
+        (wrapWithFlags pkgs.chromium cfg.flags);
 
       extensions = [
         "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
         "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # privacy badger
         "ldpochfccmkkmhdbclfhpagapcfdljkj" # decentraleyes
         "hfjbmagddngcpeloejdejnfgbamkjaeg" # vimium c
-        "cglpcedifkgalfdklahhcchnjepcckfn" # newtab adapter
-        # "klbibkeccnjlkjkiokjodocebajanakg" # the great suspender
         "hkgfoiooedgoejojocmhlaklaeopbecg" # picture-in-picture
+        "iaiomicjabeggjcfkbimgmglanimpnae" # tab sesssion manager
+        "mmcgnaachjapbbchcpjihhgjhpfcnoan" # open new tab after current tab
       ];
     };
+
+    modules.desktop.browsers.chromium.flags = [
+      # Dark theme
+      "--force-dark-mode"
+      # "--enable-features=WebUIDarkMode"
+
+      # GPU acceleration
+      "--ignore-gpu-blacklist"
+      "--enable-gpu-rasterization"
+      "--enable-native-gpu-memory-buffers"
+      "--enable-zero-copy"
+    ];
 
   };
 }
