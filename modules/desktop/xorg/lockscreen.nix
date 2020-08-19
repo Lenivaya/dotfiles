9e3e879c9c5021ef9468690e3fad6a4628e2ab92
Lenivaya <xocada@gmail.com>
2020-08-19 12:22:04 +0300

    Got rid of xautolock
    Former-commit-id: f9b046dbedc06f3ad2f875d8badbb4168507d440

--- modules/desktop/xorg/lockscreen.nix
+++ modules/desktop/xorg/lockscreen.nix
@@ -1,20 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.xautolock = {
-    enable = true;
-    time = 5;
-    locker = "${pkgs.betterlockscreen}/bin/betterlockscreen -l dim";
-    notifier = ''
-      ${pkgs.libnotify}/bin/notify-send "Locking in 10 seconds"
-    '';
-    extraOptions = [ "-lockaftersleep" "-corners ----" ];
-  };
-
-  my.packages = with pkgs.unstable; [ betterlockscreen ];
+  my.packages = with pkgs; [ betterlockscreen xidlehook ];
 
   my.home.xdg.configFile."betterlockscreenrc" = {
     source = <config/betterlockscreen/betterlockscreenrc>;
     recursive = true;
+  };
+
+  systemd.user.services.xidlehook = {
+    description = "General-purpose replacement for xautolock.";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = ''
+        ${pkgs.xidlehook}/bin/xidlehook \
+          --not-when-fullscreen \
+          --not-when-audio \
+          --timer 300 "${pkgs.betterlockscreen}/bin/betterlockscreen -l dim" \
+          ""
+      '';
+      Restart = "always";
+      RestartSec = 2;
+    };
   };
 }
