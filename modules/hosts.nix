531d36cae657a409f7177e9a8945906caa6dd567
Lenivaya <xocada@gmail.com>
2020-08-27 01:24:30 +0300

    Blocking ads module

---
+++ modules/hosts.nix
@@ -0,0 +1,21 @@
+# Blocking some internet shit here
+
+{ config, options, lib, ... }:
+with lib;
+let cfg = config.modules.hosts;
+in {
+  options.modules.hosts = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    networking.extraHosts = let
+      hostsPath =
+        "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
+      hostsFile = builtins.fetchurl hostsPath;
+    in builtins.readFile "${hostsFile}";
+  };
+}
