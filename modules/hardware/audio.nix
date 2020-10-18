08c70c49d088cd8821fb023ff300031d6a0baf27
Lenivaya <xocada@gmail.com>
2020-10-18 18:16:23 +0300

    More modules

---
+++ modules/hardware/audio.nix
@@ -0,0 +1,14 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.hardware.audio;
+in {
+  options.modules.hardware.audio = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+    user.extraGroups = [ "audio" ];
+  };
+}
