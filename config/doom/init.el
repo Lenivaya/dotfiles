f7d42a69103a3018515e4cd95993a3107101151c
Lenivaya <xocada@gmail.com>
2020-10-20 22:17:05 +0300

    A few emacs changes

--- config/doom/init.el
+++ config/doom/init.el
@@ -98,7 +98,7 @@
         +dictionary)
        lsp
        ;;macos             ; MacOS-specific commands
-       magit                               ; a git porcelain for Emacs
+       (magit +forge)                               ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        pass              ; password manager for nerds
        pdf               ; pdf enhancements
