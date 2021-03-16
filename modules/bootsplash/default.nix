70fc2f01facfc61a8b90faaecbacff4b0098fc9d
Lenivaya <xocada@gmail.com>
2021-03-16 18:55:15 +0200

    Add splash screen
    Former-commit-id: 76b42e8bf9f35d768d8f9c82166ad9271124cb6a

---
+++ modules/bootsplash/default.nix
@@ -0,0 +1,17 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.bootsplash;
+in {
+  options.modules.bootsplash = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    boot.plymouth = {
+      enable = true;
+    };
+  };
+
+}
