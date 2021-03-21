7ac647cb06a3cb06bb234bffd227f8ffbe6ef5d3
Lenivaya <xocada@gmail.com>
2021-03-21 21:53:10 +0200

    [DEV] Add elixir module

---
+++ modules/develop/elixir.nix
@@ -0,0 +1,16 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.elixir;
+in {
+  options.modules.dev.elixir = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs;
+      [
+        erlang elixir
+      ];
+  };
+
+}
