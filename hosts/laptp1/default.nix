4df720bc20c59fc760228a728a65ec0472b34a5b
Lenivaya <xocada@gmail.com>
2021-03-26 08:53:12 +0200

    Update laptp1 config
    Former-commit-id: 359e15bba0b97153869ef5db0d5667b47e55d4ec

--- hosts/laptp1/default.nix
+++ hosts/laptp1/default.nix
@@ -62,6 +62,7 @@
       haskell.enable = true;
       node.enable = true;
       python.enable = true;
+      elixir.enable = true;
     };
 
     services = {
@@ -90,23 +91,10 @@
 
   services.xserver = {
     deviceSection = ''
-      Option "AccelMethod" "glamor"
       Option "TearFree" "on"
     '';
-    useGlamor = true;
   };
-
-  # Fix weird graphical glitches
-  # https://github.com/NixOS/nixpkgs/issues/86212#issuecomment-64023258
-  # hardware.opengl.package = (import (pkgs.fetchzip {
-  #   name = "old-nixpkgs";
-  #   url =
-  #     "https://github.com/NixOS/nixpkgs/archive/0a11634a29c1c9ffe7bfa08fc234fef2ee978dbb.tar.gz";
-  #   sha256 = "0vj5k3djn1wlwabzff1kiiy3vs60qzzqgzjbaiwqxacbvlrci10y";
-  # }) { localSystem = "x86_64-linux"; }).mesa.drivers;
-
-  # services.xserver.videoDrivers = [ "modesetting" ];
-  # environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
 
   # Optimize power use
   environment.systemPackages = [ pkgs.acpi ];
@@ -114,5 +102,4 @@
 
   # Kernel
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_lqx;
-  # boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
 }
