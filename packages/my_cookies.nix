7be6baa3401adb0db53818463f65c32024c2a5c7
Lenivaya <xocada@gmail.com>
2021-04-15 10:25:03 +0300

    [EMACS] add leetcode integration

--- packages/my_cookies.nix
+++ packages/my_cookies.nix
@@ -1,14 +1,20 @@
-{ lib, buildPythonPackage, fetchPypi, }:
+{ lib, pkgs }:
+
+with pkgs.python3Packages;
 
 buildPythonPackage rec {
   pname = "my_cookies";
   version = "0.1.1";
 
-  src = fetchPypi { inherit pname version; };
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "SDXfJoMCR+cPXt/SkTVjkddpY3dQ4/WjKtwlxUG727A=";
+  };
+
+  propagatedBuildInputs = [ browser-cookie3 ];
 
   meta = with lib; {
-    description =
-      "Package for retrieving leetcode cookies from Chrome with local keyring.";
+    description = "Package for retrieving leetcode cookies from browser";
     platforms = platforms.unix;
   };
 }
