e1453668e787825e2d7906aa2c82be1ca120ad29
Lenivaya <xocada@gmail.com>
2021-04-19 16:26:55 +0300

    Disable some unneeded things in intel module
    Former-commit-id: 41e0d3946dbe8d02facf863ce5d8931023adcbe7

--- modules/hardware/cpu/intel.nix
+++ modules/hardware/cpu/intel.nix
@@ -18,17 +18,6 @@ in
     ];
 
     boot.kernelParams = [
-      # Enable power-saving display C-states
-      "i915.enable_dc=1"
-      # Enable frame buffer compression for power savings
-      "i915.enable_fbc=1"
-      # Enable PSR
-      "i915.enable_psr=1"
-      # Display power wells are always on
-      "i915.disable_power_well=0"
-      # Enable GuC load for GuC submission and/or HuC load
-      "i915.enable_guc=-1"
-
       "intel_iommu=on"
       "iommu=pt"
 
