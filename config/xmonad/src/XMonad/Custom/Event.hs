0b3972613ee4f6cefd1b7bbe2b9d45bcc5979aa0
Lenivaya <xocada@gmail.com>
2021-04-22 23:33:49 +0300

    [XMonad|Emacs] Make emacs-everywhere appear as floating window in XMonad
    Former-commit-id: cda7228ca03146bc8064decb00fcf6ebfe38ebf1

--- config/xmonad/src/XMonad/Custom/Event.hs
+++ config/xmonad/src/XMonad/Custom/Event.hs
@@ -4,20 +4,26 @@ module XMonad.Custom.Event
 
 import qualified Data.Map.Strict               as M
 import           Data.Monoid
-import           XMonad                  hiding ( handleEventHook )
+import           XMonad                  hiding ( handleEventHook
+                                                , manageHook
+                                                )
 import           XMonad.Custom.Scratchpads
+import           XMonad.Hooks.DynamicProperty
 import           XMonad.Hooks.EwmhDesktops
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Hooks.RefocusLast
 import           XMonad.Util.Loggers.NamedScratchpad
+
+import           XMonad.Custom.Manage           ( manageHook )
 
 -- Keeps last focused window
 myPred = refocusingIsActive <||> isFloat
 
 handleEventHook :: Event -> X All
 handleEventHook = mconcat
-  [ nspTrackHook scratchpads
+  [ dynamicTitle manageHook
   , docksEventHook
   , fullscreenEventHook
+  , nspTrackHook scratchpads
   , refocusLastWhen myPred
   ]
