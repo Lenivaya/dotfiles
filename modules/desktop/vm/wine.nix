5c59190a3b29b524e22bbe6517f7efaf9035beac
Lenivaya <xocada@gmail.com>
2021-06-07 18:38:07 +0300

    Add wine module
    Former-commit-id: 26f9c94f58e2766a745e3552e1790730870a8bb6

---
+++ modules/desktop/vm/wine.nix
@@ -0,0 +1,13 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.vm.wine;
+in {
+  options.modules.desktop.vm.wine.enable = mkBoolOpt false;
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs;
+      [ (winetricks.override { wine = wineWowPackages.staging; }) ];
+  };
+}
