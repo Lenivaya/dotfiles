401aa8bdd5bd380f6d5a9934e338f422e3e22d10
Lenivaya <xocada@gmail.com>
2021-08-07 10:15:34 +0300

    configure nvidia gpu
    Former-commit-id: 9a502795c3851283fbb55599c6630364fa9cc650

--- hosts/t440p/default.nix
+++ hosts/t440p/default.nix
@@ -99,10 +99,30 @@
     bootsplash = { enable = true; };
   };
 
-  # services.xserver = { videoDrivers = [ "modesetting" "nvidia" ]; };
+  services.xserver = { videoDrivers = [ "nvidia" ]; };
+  hardware = {
+    nvidia = {
+      nvidiaPersistenced = true;
+      modesetting.enable = true;
+      prime = {
+        offload.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:2:0:0";
+      };
+    };
+  };
+  environment.systemPackages = let
+    nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec -a "$0" "$@"
+    '';
+  in [ nvidia-offload ];
 
   # Optimize power use
-  environment.systemPackages = [ pkgs.acpi ];
+  # environment.systemPackages = [ pkgs.acpi ];
   powerManagement.powertop.enable = true;
 
   networking.useDHCP = false;
