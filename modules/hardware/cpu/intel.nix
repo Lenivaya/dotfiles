fb3db95c9e8c84d19ed5aa5cc7ce4fbf85dcf7f0
Lenivaya <xocada@gmail.com>
2021-08-08 09:49:04 +0300

    update in cpu/intel module

--- modules/hardware/cpu/intel.nix
+++ modules/hardware/cpu/intel.nix
@@ -1,10 +1,9 @@
-{ config, options, lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 with lib;
 with lib.my;
 let cfg = config.modules.hardware.cpu.intel;
-in
-{
+in {
   options.modules.hardware.cpu.intel.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
@@ -30,20 +29,23 @@ in
       "snd-hda-intel.enable_msi=1"
     ];
 
-    hardware.cpu.intel.updateMicrocode = true;
-
-    hardware.opengl.extraPackages = with pkgs; [
-      (vaapiIntel.override { enableHybridCodec = true; })
-      intel-media-driver
-      intel-ocl
-      intel-compute-runtime
-    ];
+    # boot.kernel.sysfs.sys.devices.system.cpu = {
+    #   # Enabling energy-efficiency optimizations may limit maximum operating frequency with or without the HWP feature.  With HWP enabled, the optimizations are done only in the turbo frequency range.  Without it, they are done in the entire available frequency range.
+    #   intel_pstate.energy_efficiency = true;
+    #   # It causes the minimum P-state limit to be increased dynamically for a short time whenever a task previously waiting on I/O is selected to run on a given logical CPU (the purpose of this mechanism is to improve performance).
+    #   intel_pstate.hwp_dynamic_boost = true;
+    #   # Minimum time (in microseconds) that has to pass between two consecutive runs of governor computations
+    #   cpufreq.schedutil.rate_limit_us = 0;
+    # };
 
     environment.systemPackages = with pkgs; [
       pcm
       config.boot.kernelPackages.intel-speed-select
-      libva-utils
     ];
-  };
 
+    hardware.cpu.intel.updateMicrocode = true;
+    hardware.opengl.extraPackages = with pkgs; [ intel-ocl ];
+
+    services.throttled.enable = lib.mkDefault true;
+  };
 }
