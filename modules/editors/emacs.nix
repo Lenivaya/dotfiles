a750ddb2add5d632ff4a462a8b693edda98e98c4
Lenivaya <xocada@gmail.com>
2021-08-09 20:17:30 +0300

    fix package name
    Former-commit-id: 1e7ccfed3250255d49f5753392be0678e9f38c28

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -74,7 +74,7 @@ in
       programs.emacs = {
         enable = true;
         package = pkgs.emacsGcc; # 28 + native-comp
-        # pkgs.emacPgtksGcc; # 28 + pgtk + native-comp
+        # pkgs.emacPgtkGcc; # 28 + pgtk + native-comp
         extraPackages = epkgs:
           [
             # :term vterm
