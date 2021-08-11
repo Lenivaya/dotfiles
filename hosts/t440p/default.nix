5d079829e5c5a8a862daeb3f88fa6bd1a68a6dd4
Lenivaya <xocada@gmail.com>
2021-08-11 19:24:10 +0300

    update t440p config
    Former-commit-id: 9247000c1cbf7bbb0f6f4c4005b5034889a1b7e1

--- hosts/t440p/default.nix
+++ hosts/t440p/default.nix
@@ -85,6 +85,7 @@
       profiles.laptop.enable = true;
       cpu.intel.enable = true;
       # gpu.intel.enable = true;
+      gpu.nvidia.enable = true;
       fs = {
         ssd.enable = true;
         enable = true;
@@ -100,11 +101,10 @@
     bootsplash = { enable = true; };
   };
 
-  services.xserver = { videoDrivers = [ "nvidia" ]; };
   hardware = {
     nvidia = {
-      nvidiaPersistenced = true;
-      modesetting.enable = true;
+      # nvidiaPersistenced = true;
+      # modesetting.enable = true;
       prime = {
         offload.enable = true;
         intelBusId = "PCI:0:2:0";
@@ -112,17 +112,15 @@
       };
     };
   };
-  environment.systemPackages =
-    let
-      nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-        export __NV_PRIME_RENDER_OFFLOAD=1
-        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-        export __GLX_VENDOR_LIBRARY_NAME=nvidia
-        export __VK_LAYER_NV_optimus=NVIDIA_only
-        exec -a "$0" "$@"
-      '';
-    in
-    [ nvidia-offload ];
+  environment.systemPackages = let
+    nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec -a "$0" "$@"
+    '';
+  in [ nvidia-offload ];
 
   networking.useDHCP = false;
   networking.interfaces.enp0s25.useDHCP = true;
