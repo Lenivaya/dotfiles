46ecaafe2d83dfa2ac485a59de3a71aacfdaa1f0
Lenivaya <xocada@gmail.com>
2021-08-14 00:11:10 +0300

    [picom] more configuration
    Former-commit-id: e7497a434ce27993b7f41827c258b3227d0161fd

--- modules/desktop/xorg/compositor.nix
+++ modules/desktop/xorg/compositor.nix
@@ -42,7 +42,23 @@
 
       shadow-radius = 7;
 
+      # Unredirect all windows if a full-screen opaque window is detected, to
+      # maximize performance for full-screen windows. Known to cause
+      # flickering when redirecting/unredirecting windows.
       unredir-if-possible = true;
+
+      # GLX backend: Avoid using stencil buffer, useful if you don't have a
+      # stencil buffer. Might cause incorrect opacity when rendering
+      # transparent content (but never practically happened) and may not work
+      # with blur-background. My tests show a 15% performance boost.
+      # Recommended.
+      glx-no-stencil = true;
+
+      # Use X Sync fence to sync clients' draw calls, to make sure all draw
+      # calls are finished before picom starts drawing. Needed on
+      # nvidia-drivers with GLX backend for some users.
+      xrender-sync-fence = true;
+
       dbe = true;
     '';
   };
