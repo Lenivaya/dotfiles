a4cf2da9dccb2e6c0cae3ebf717d4324cff71c53
Lenivaya <xocada@gmail.com>
2021-08-23 21:12:28 +0300

    update hosts blocking module
    Former-commit-id: cb748d2209e7dc0c2316481c7202986579959f13

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,24 +1,28 @@
 # Blocking some internet shit here
 
-{ config, options, lib, pkgs, ... }:
+{ config, options, lib, pkgs, inputs, ... }:
 
 with lib;
 with lib.my;
 let cfg = config.modules.hosts;
-in
-{
+in {
   options.modules.hosts.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
-    networking.extraHosts =
-      let
-        hostsFile = pkgs.fetchFromGitHub {
-          owner = "StevenBlack";
-          repo = "hosts";
-          rev = "c202dbda759bb0ab52c68e9f675ccd2ad4b59c3e";
-          sha256 = "etXlrCUOBU2U/T/lHYOzAtIyLW4k1OXA7Q/+WMvPiZg=";
-        };
-      in
-      builtins.readFile "${hostsFile}/hosts";
+    nixpkgs.overlays = [ inputs.nur.overlay ];
+    environment.systemPackages = with pkgs;
+      [ nur.repos.ambroisie.unified-hosts-lists ];
+
+    # networking.extraHosts =
+    #   let
+    #     hostsFile = pkgs.fetchFromGitHub {
+    #       owner = "StevenBlack";
+    #       repo = "hosts";
+    #       rev = "c202dbda759bb0ab52c68e9f675ccd2ad4b59c3e";
+    #       sha256 = "etXlrCUOBU2U/T/lHYOzAtIyLW4k1OXA7Q/+WMvPiZg=";
+    #     };
+    #   in
+    #   builtins.readFile "${hostsFile}/hosts";
+
   };
 }
