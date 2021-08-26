e7e59631f282f2f87006baef0a1da345d1e71de9
Lenivaya <xocada@gmail.com>
2021-08-26 22:30:34 +0300

    update hosts blocking module
    Former-commit-id: 08527c1ad9799ba8e8a2577827436ef0dd80bcf2

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -9,9 +9,37 @@ in {
   options.modules.hosts.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
+
     nixpkgs.overlays = [ inputs.nur.overlay ];
-    environment.systemPackages = with pkgs;
-      [ nur.repos.ambroisie.unified-hosts-lists ];
+
+    services.unbound = {
+      enable = true;
+
+      settings = {
+        server = {
+          access-control = [ "127.0.0.0/24 allow" ];
+
+          interface = [ "0.0.0.0" "::" ];
+
+          so-reuseport = true;
+          tls-cert-bundle = "/etc/ssl/certs/ca-certificates.crt";
+          tls-upstream = true;
+
+          include = "${pkgs.nur.repos.ambroisie.unbound-zones-adblock}/hosts";
+        };
+
+        forward-zone = [{
+          name = ".";
+          forward-addr = [
+            "1.1.1.1@853#cloudflare-dns.com"
+            "1.0.0.1@853#cloudflare-dns.com"
+          ];
+        }];
+      };
+    };
+
+    # environment.systemPackages = with pkgs;
+    #   [ nur.repos.ambroisie.unified-hosts-lists ];
 
     # networking.extraHosts =
     #   let
