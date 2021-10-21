34be09040199a38374e9b687c99fe3a72a4819c2
Lenivaya <xocada@gmail.com>
2021-10-21 20:42:13 +0300

    add caffeinate package
    Former-commit-id: e882976ed154683bc2100fa7537fd30d91ed7a11

---
+++ packages/caffeinate.nix
@@ -0,0 +1,24 @@
+{ stdenv, requireFile, unzip, pkgs }:
+
+with stdenv;
+with pkgs;
+
+rustPlatform.buildRustPackage rec {
+  pname = "caffeinate";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "rschmukler";
+    repo = pname;
+    rev = "e20985a4b630eb5c76e16c2547da0aba65f097d5";
+    sha256 = "1p9k9dz152igw2mazf8m9ac16hh381q8nqxbdahb0dn0npx10s6v";
+  };
+
+  cargoSha256 = "tZ3XVrlIs3xZvPiTU4fchus2lgenfVPB+xz0PhVu9gg=";
+
+  meta = with stdenv.lib; {
+    description =
+      "A command-line app bringing caffeinate functionality to xidlehook.";
+    homepage = "https://github.com/rschmukler/caffeinate";
+  };
+}
