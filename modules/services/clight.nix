ba7d9422f233bac8e7905cf89be19ab5f078999a
Lenivaya <xocada@gmail.com>
2021-12-05 23:32:08 +0200

    configurations for the clight service

--- modules/services/clight.nix
+++ modules/services/clight.nix
@@ -4,27 +4,29 @@
   services.clight = {
     enable = lib.mkDefault true;
 
-    temperature = {
-      day = 3000;
-      night = 2400;
-    };
-
     settings = {
       verbose = true;
 
-      backlight.ac_timeouts = [ 120 300 60 ];
-      backlight.pause_on_lid_closed = true;
+      backlight = {
+        ac_timeouts = [ 120 300 60 ];
+        pause_on_lid_closed = true;
+        capture_on_lid_opened = true;
+      };
 
-      sensor.ac_regression_points =
-        [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.8 ];
-      sensor.batt_regression_points =
-        [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.8 ];
-      sensor.captures = [ 16 16 ];
+      sensor = {
+        ac_regression_points =
+          [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.8 ];
+        batt_regression_points =
+          [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.8 ];
+        captures = [ 16 16 ];
+      };
 
       gamma.long_transition = true;
 
-      screen.contrib = 0.2;
-      screen.num_samples = 20;
+      screen = {
+        contrib = 0.2;
+        num_samples = 20;
+      };
     };
   };
 
