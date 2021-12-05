fabbd6b386d7efd96e3db2e5dbdb789a073769b5
Lenivaya <xocada@gmail.com>
2021-12-06 00:29:10 +0200

    delete xmonad overlay
    Former-commit-id: 055721567bfa25c87a0817948885dd624e527004

--- overlays/xmonad.nix
+++
@@ -1,9 +0,0 @@
-(self: super: {
-  haskellPackages = super.haskellPackages.override {
-    overrides = hself: hsuper: {
-      # xmonad-contrib = hsuper.xmonad-contrib_0_17_0;
-      # xmonad = hsuper.xmonad_0_17_0;
-      # xmonad-extras = hsuper.xmonad-extras_0_17_0;
-    };
-  };
-})
