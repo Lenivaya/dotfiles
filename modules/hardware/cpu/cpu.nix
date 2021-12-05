e385d09b725469895bf2ae6bce15c283c4077451
Lenivaya <xocada@gmail.com>
2021-12-05 23:46:47 +0200

    remove unneeded options for hardware.cpu module
    Former-commit-id: fc8adc2184576bbf9670b3916691c4aa69141af8

--- modules/hardware/cpu/cpu.nix
+++ modules/hardware/cpu/cpu.nix
@@ -1,24 +1,15 @@
 { config, lib, ... }:
 
 with lib;
+with lib.my;
 
 let cfg = config.modules.hardware.cpu;
-in {
+in
+{
   options.modules.hardware.cpu = {
 
-    tdp = {
-      nominal = mkOption {
-        type = types.int;
-        default = 0;
-      };
-
-      up = mkOption {
-        type = types.int;
-        default = 0;
-      };
-    };
-
     undervolt = {
+      enable = mkBoolOpt false;
       core = mkOption {
         type = types.int;
         default = 0;
@@ -52,67 +43,14 @@ in {
     modules.hardware.cpu.undervolt.cache = cfg.undervolt.core;
 
     # Compatibility with intel-undervolt
-    services.undervolt = with cfg.undervolt; {
-      coreOffset = core;
-      gpuOffset = gpu;
-      uncoreOffset = uncore;
-      analogioOffset = analogio;
-    };
-
-    # Compatibility with throttled
-    services.throttled.extraConfig = let
-      mkValueString = v:
-        if builtins.isFloat v then
-          toString v
-        else if true == v then
-          "True"
-        else if false == v then
-          "False"
-        else
-          generators.mkValueStringDefault { } v;
-      mkKeyValue = generators.mkKeyValueDefault { inherit mkValueString; } ":";
-      toConf = generators.toINI { inherit mkKeyValue; };
-
-      settings = {
-        GENERAL = {
-          Enabled = true;
-          Sysfs_Power_Path = "/sys/class/power_supply/AC*/online";
-        };
-
-        BATTERY = {
-          Update_Rate_s = 30;
-          PL1_Tdp_W = cfg.tdp.nominal - 5;
-          PL1_Duration_s = 30;
-          PL2_Tdp_W = cfg.tdp.nominal + 5;
-          PL2_Duration_S = 5.0e-3;
-          Trip_Temp_C = 80;
-          cTDP = 0;
-          Disable_BDPROCHOT = false;
-        };
-
-        AC = {
-          Update_Rate_s = 5;
-          PL1_Tdp_W = cfg.tdp.up - 5;
-          PL1_Duration_s = 45;
-          PL2_Tdp_W = cfg.tdp.up + 5;
-          PL2_Duration_S = 7.0e-3;
-          Trip_Temp_C = 95;
-          HWP_Mode = true;
-          cTDP = 0;
-          Disable_BDPROCHOT = false;
-        };
-
-        # In theory, the undervolting can be more aggressive since the cpu isn't as stressed
-        "UNDERVOLT.BATTERY" = settings."UNDERVOLT.AC";
-
-        "UNDERVOLT.AC" = with cfg.undervolt; {
-          CORE = core;
-          GPU = gpu;
-          CACHE = cache;
-          UNCORE = uncore;
-          ANALOGIO = analogio;
-        };
+    services.undervolt = with cfg.undervolt;
+      mkIf enable {
+        enable = enable;
+        coreOffset = core;
+        gpuOffset = gpu;
+        uncoreOffset = uncore;
+        analogioOffset = analogio;
       };
-    in toConf settings;
+
   };
 }
