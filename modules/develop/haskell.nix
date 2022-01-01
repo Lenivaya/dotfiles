e0b07b2a6fbc65f06ae487b2b3d14d44fa61d07c
Lenivaya <xocada@gmail.com>
2022-01-01 15:28:41 +0200

    [dev/haskell] some formatter configurations
    Former-commit-id: 440b30c494ac6abc3511cfb466cf9833713723be

--- modules/develop/haskell.nix
+++ modules/develop/haskell.nix
@@ -3,8 +3,7 @@
 with lib;
 with lib.my;
 let cfg = config.modules.dev.haskell;
-in
-{
+in {
   options.modules.dev.haskell.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
@@ -16,8 +15,12 @@ in
           with ps; [
             stack
             cabal-install
+
+            # Linter and formatters
             hlint
             brittany
+            ormolu
+
             hasktags
             haskell-language-server
           ]))
