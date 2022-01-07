ea082bd50d31ec6e789c52c3586784423baca82b
Lenivaya <xocada@gmail.com>
2022-01-07 23:00:45 +0200

    add some mpv config

--- modules/desktop/media/mpv.nix
+++ modules/desktop/media/mpv.nix
@@ -2,14 +2,20 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.media.mpv;
-in
-{
+let
+  cfg = config.modules.desktop.media.mpv;
+  configDir = config.dotfiles.configDir;
+in {
   options.modules.desktop.media.mpv.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
     nixpkgs.overlays = mkIf config.modules.desktop.browsers.firefox.enable
       [ inputs.nur.overlay ];
+
+    home.configFile."mpv" = {
+      source = "${configDir}/mpv";
+      recursive = true;
+    };
 
     user.packages = with pkgs; [
       mpv-with-scripts
@@ -19,6 +25,7 @@ in
         (nur.repos.ambroisie.ff2mpv-go.overrideAttrs (oldAttr: {
           postInstall = ''
             mv $out/bin/ff2mpv-go $out/bin/ff2mpv
+            # TODO make manifest generation workable for user
             mkdir -p "$out/lib/mozilla/native-messaging-hosts"
             $out/bin/ff2mpv --manifest > "$out/lib/mozilla/native-messaging-hosts/ff2mpv.json"
           '';
