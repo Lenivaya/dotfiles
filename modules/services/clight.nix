7ab7587282bca677eae87af0f88db03f7958188a
Lenivaya <xocada@gmail.com>
2022-05-12 14:59:11 +0300

    [clight] disable dimming
    Former-commit-id: e4a93f577d9cbaa8a11dfab04b9c41cb2eeef515

--- modules/services/clight.nix
+++ modules/services/clight.nix
@@ -13,6 +13,7 @@
         capture_on_lid_opened = true;
       };
 
+      dimmer.disabled = true;
       # sensor = {
       #   ac_regression_points =
       #     [ 0.0 0.12 0.18 0.28 0.42 0.49 0.54 0.65 0.75 0.78 0.8 ];
