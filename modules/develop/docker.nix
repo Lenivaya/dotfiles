3be87a81c5c2af024d36fd26204787530040939f
Lenivaya <xocada@gmail.com>
2022-05-12 15:01:31 +0300

    add docker-compose to docker module
    Former-commit-id: 5c69d0a43d732a7b11b5fb8e6a7fb26c35d7b008

--- modules/develop/docker.nix
+++ modules/develop/docker.nix
@@ -1,6 +1,8 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
-{
+with lib.my; {
   virtualisation.docker.enableOnBoot = false;
   virtualisation.docker.enable = true;
+  user.extraGroups = [ "docker" ];
+  user.packages = with pkgs; [ docker-compose_2 ];
 }
