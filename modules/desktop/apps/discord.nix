f9598c6e2763bf88f951587f4c4c35a57a9728d6
Lenivaya <xocada@gmail.com>
2022-06-07 12:19:44 +0300

    use discord from discord overlay
    Former-commit-id: 8f80205b513fb60fcce1e4b62afb07faed0d5265

--- modules/desktop/apps/discord.nix
+++ modules/desktop/apps/discord.nix
@@ -1,18 +1,24 @@
-{ config, options, lib, pkgs, ... }:
+{ config, options, lib, pkgs, inputs, ... }:
 
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.apps.discord;
-in
-{
+in {
   options.modules.desktop.apps.discord.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
+    nixpkgs.overlays = with inputs; [
+      discord-overlay.overlay
+      discocss.overlay
+    ];
+
     user.packages = with pkgs;
       [
         # If not installed from unstable, Discord will sometimes soft-lock itself
         # on a "there's an update for discord" screen.
-        unstable.discord
+
+        discord
+        discocss
       ];
   };
 }
