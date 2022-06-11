79cc82f8183ee0572e63ec5071d22fb8c9717257
Lenivaya <xocada@gmail.com>
2022-06-12 01:16:53 +0300

    add precommit hooks

--- shell.nix
+++ shell.nix
@@ -1,15 +1,15 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { }, pre-commit-hook }:
 
 with pkgs;
 let
   nixBin = writeShellScriptBin "nix" ''
     ${nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
   '';
-in
-mkShell {
+in mkShell {
   buildInputs = [ git gnupg nix-zsh-completions ];
   shellHook = ''
     export DOTFILES="$(pwd)"
     export PATH="$DOTFILES/bin:${nixBin}/bin:$PATH"
+    ${pre-commit-hook}
   '';
 }
