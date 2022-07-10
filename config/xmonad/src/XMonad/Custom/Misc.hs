e54a887d6d960791c79d6c3e07c1b16adaf2ed53
Lenivaya <xocada@gmail.com>
2022-07-10 12:56:42 +0300

    don't expicitly set rofi theme

--- config/xmonad/src/XMonad/Custom/Misc.hs
+++ config/xmonad/src/XMonad/Custom/Misc.hs
@@ -31,7 +31,7 @@ applications = Applications
   , top               = "htop"
   , reader            = "zathura"
   , editor            = "$EDITOR"
-  , appmenu           = "rofi -show drun -theme main"
+  , appmenu           = "rofi -show drun"
   , clipboardSelector =
-    "rofi -theme main -modi 'clipboard:greenclip print' -show clipboard -run-command '{cmd}'"
+    "rofi -modi 'clipboard:greenclip print' -show clipboard -run-command '{cmd}'"
   }
