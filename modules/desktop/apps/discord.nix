5ec2af8732cc12070fa0919afb8ce932952b092a
Lenivaya <xocada@gmail.com>
2022-08-10 12:16:40 +0300

    add discord-replugged
    Former-commit-id: 0111bc42b1356f269c0df9bba3f71423b8a87112

--- modules/desktop/apps/discord.nix
+++ modules/desktop/apps/discord.nix
@@ -15,15 +15,43 @@ in {
   config = mkIf cfg.enable {
     nixpkgs.overlays = with inputs; [
       discord-overlay.overlay
-      discocss.overlay
+      # discocss.overlay
     ];
 
-    user.packages = with pkgs; [
-      # If not installed from unstable, Discord will sometimes soft-lock itself
-      # on a "there's an update for discord" screen.
-
+    user.packages = with pkgs; let
+      discord =
+        # discord-plugged.override
+        inputs.replugged.lib.makeDiscordPlugged
+        {
+          inherit pkgs;
+          plugins = {
+            inherit
+              (inputs)
+              discord-tweaks
+              discord-image-tools
+              discord-push-fix
+              discord-better-status-indicators
+              discord-multitask
+              discord-view-raw
+              discord-channel-typing
+              ;
+          };
+          # themes = with inputs; [
+          #   # discord-tokyonight
+          #   # discord-crearts
+          #   # discord-surcord
+          # ];
+        };
+    in [
+      # discord
+      # discocss
       discord
-      discocss
+      (writeScriptBin
+        "discord"
+        ''
+          #!${stdenv.shell}
+          exec discordcanary
+        '')
     ];
   };
 }
