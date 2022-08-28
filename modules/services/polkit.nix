265c0f595d0c9f8436baa7ad72ab820897ce1fba
Lenivaya <xocada@gmail.com>
2022-08-28 12:08:58 +0300

    add polkit service module

---
+++ modules/services/polkit.nix
@@ -0,0 +1,25 @@
+  # GUI for polkit
+  {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: {
+    security.polkit.enable = true;
+    systemd = {
+      user.services.polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wants = ["graphical-session.target"];
+        wantedBy = ["graphical-session.target"];
+        after = ["graphical-session.target"];
+
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+      };
+    };
+  }
