949472bd4a42ea9366ef9097fdf56f5bec8b04dc
Lenivaya <xocada@gmail.com>
2022-09-08 11:58:41 +0300

    updates to tlp module

--- modules/services/tlp.nix
+++ modules/services/tlp.nix
@@ -1,4 +1,4 @@
-_: {
+{config, ...}: {
   services.tlp.enable = true;
   services.tlp.settings = {
     # Set CPU governors
@@ -22,5 +22,25 @@ _: {
     # Use minimum amount of cores on battery
     SCHED_POWERSAVE_ON_AC = 0;
     SCHED_POWERSAVE_ON_BAT = 1;
+
+    PLATFORM_PROFILE_ON_AC = "performance";
+    PLATFORM_PROFILE_ON_BAT = "low-power";
+    AHCI_RUNTIME_PM_ON_AC = "auto";
+    AHCI_RUNTIME_PM_ON_BAT = "auto";
+    PCIE_ASPM_ON_AC = "performance";
+    PCIE_ASPM_ON_BAT = "powersupersave";
+    RUNTIME_PM_ON_AC = "auto";
+    RUNTIME_PM_ON_BAT = "auto";
   };
+
+  environment.systemPackages = [config.boot.kernelPackages.x86_energy_perf_policy];
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        tlp = prev.tlp.override {
+          inherit (config.boot.kernelPackages) x86_energy_perf_policy;
+        };
+      }
+    )
+  ];
 }
