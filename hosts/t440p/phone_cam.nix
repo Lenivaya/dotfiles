98a836a185650401847a9635e87d9707608c9824
Lenivaya <xocada@gmail.com>
2022-11-07 19:46:46 +0200

    [t440p] add module for using virtual camera

--- hosts/t440p/phone_cam.nix
+++ hosts/t440p/phone_cam.nix
@@ -1,9 +1,13 @@
+# Virtual camera and mic
 {
   config,
   lib,
   pkgs,
   ...
 }: {
-  boot.kernelModules = ["v4l2loopback"];
-  boot.extraModprobeConfig = "options v4l2loopback exclusive_caps=1";
+  boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback.out];
+  boot.kernelModules = ["v4l2loopback" "snd-aloop"];
+  boot.extraModprobeConfig = ''options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"'';
 }
+# ffmpeg -hide_banner -vcodec mjpeg -framerate 60 -s 640x480 -i /dev/video1 -vcodec rawvideo -vf format=yuv420p,"boxblur=1" -f v4l2 /dev/video0
+# ffmpeg -hide_banner -vcodec mjpeg -framerate 60 -s 2560x1440 -i /dev/video1 -vcodec rawvideo -vf format=yuv420p,"boxblur=1" -f v4l2 /dev/video0
