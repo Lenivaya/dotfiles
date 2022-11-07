47ffcc7cd2bb1ae7b41539583705eb699725602c
Lenivaya <xocada@gmail.com>
2022-11-07 19:51:38 +0200

    [qemu] make module usable
    Former-commit-id: 9d23ca8886b6329ad50f3a76a0d8b40f30f7a1ad

--- modules/desktop/vm/qemu.nix
+++ modules/desktop/vm/qemu.nix
@@ -11,8 +11,21 @@ with lib.my; let
 in {
   options.modules.desktop.vm.qemu.enable = mkBoolOpt false;
 
-  config =
-    mkIf cfg.enable {environment.systemPackages = with pkgs; [qemu];};
+  config = mkIf cfg.enable {
+    boot.extraModprobeConfig = "options kvm_intel nested=1";
+    virtualisation.libvirtd.enable = true;
+    programs.dconf.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      qemu
+      virt-viewer
+      virt-manager
+      spice-vdagent
+      bridge-utils
+    ];
+
+    user.extraGroups = ["libvirtd"];
+  };
 }
 # Creating an image:
 #   qemu-img create -f qcow2 disk.img
