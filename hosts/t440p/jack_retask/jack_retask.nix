90ea4f3bdbb95161ae9b4ccfc238312c7316330b
Lenivaya <xocada@gmail.com>
2022-11-13 15:45:45 +0200

    [t440p] fix jack retask module
    Former-commit-id: cba14c62db8a5e6d1589db4b0f071bdd845f5b1c

--- hosts/t440p/jack_retask/jack_retask.nix
+++ hosts/t440p/jack_retask/jack_retask.nix
@@ -6,7 +6,7 @@
   pkgs,
   ...
 }: let
-  soundCard = "/sys/class/sound/hwC1D0";
+  soundCard = "/sys/class/sound/hwC2D0";
 in {
   # hardware.firmware = [ ( pkgs.writeTextDir "/lib/firmware/hda-jack-retask.fw" ( builtins.readFile ./hda-jack-retask.fw ) ) ];
   systemd.services.jack-retask = {
@@ -15,17 +15,7 @@ in {
 
     script = ''
       (
-        echo 0x12 0x90a60130
-        echo 0x13 0x40000000
-        echo 0x14 0x90170110
-        echo 0x15 0x0321101f
-        echo 0x16 0x21211010
-        echo 0x18 0x411111f0
-        echo 0x19 0x21a11010
         echo 0x1a 0x40a000f0
-        echo 0x1b 0x411111f0
-        echo 0x1d 0x40738105
-        echo 0x1e 0x411111f0
       )>${soundCard}/user_pin_configs
       echo 1 > ${soundCard}/reconfig
     '';
