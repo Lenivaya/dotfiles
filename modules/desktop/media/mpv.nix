d4acf1c1e906f6ebdfae6a37ca5ff26de35b19f9
Lenivaya <xocada@gmail.com>
2022-12-01 22:21:01 +0200

    [mpv] add some scripts

--- modules/desktop/media/mpv.nix
+++ modules/desktop/media/mpv.nix
@@ -14,17 +14,27 @@ in {
   options.modules.desktop.media.mpv.enable = mkBoolOpt false;
 
   config = mkIf cfg.enable {
-    nixpkgs.overlays =
-      mkIf config.modules.desktop.browsers.firefox.enable
-      [inputs.nur.overlay];
-
     home.configFile."mpv" = {
       source = "${configDir}/mpv";
       recursive = true;
     };
 
     user.packages = with pkgs; [
-      mpv-with-scripts
+      # mpv-with-scripts
+      (
+        mpv.override
+        {
+          scripts = with mpvScripts; [
+            autoload
+            cutter
+            convert
+            thumbnail
+            sponsorblock
+            youtube-quality
+          ];
+        }
+      )
+
       mpvc # CLI controller for mpv
       (mkIf config.services.xserver.enable celluloid) # nice GTK GUI for mpv
     ];
