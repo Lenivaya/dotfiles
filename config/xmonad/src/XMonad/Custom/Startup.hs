166df3ecae51e5a24d7aaeb571562e0f1256ef70
Lenivaya <xocada@gmail.com>
2022-12-04 23:42:21 +0200

    [xmonad] migrate from dynamiclog to statusbar

--- config/xmonad/src/XMonad/Custom/Startup.hs
+++ config/xmonad/src/XMonad/Custom/Startup.hs
@@ -4,39 +4,14 @@ module XMonad.Custom.Startup
 
 import           Control.Monad
 import           Data.Maybe
-import           Graphics.Gloss.Interface.Environment
-import           Text.Printf
 import           XMonad                  hiding ( startupHook )
-import           XMonad.Hooks.ManageDocks
+import           XMonad.Custom.Log
 import           XMonad.Hooks.SetWMName
-import           XMonad.Util.SpawnNamedPipe
 import           XMonad.Util.SpawnOnce
-
-percentFromNumber :: Int -> Float -> Int
-percentFromNumber n p = round (realToFrac n * realToFrac (p / 100))
-
-generateBarPosition :: (Int, Int) -> String -> String
-generateBarPosition (w, h) pos | pos == "top" = printf position topBarYpos width
-                               | pos == "bot" = printf position botBarYpos width
- where
-  position   = "'Static { xpos = 12 , ypos = %d , width = %d , height = 32}'"
-  width      = w - 32
-  botBarYpos = (h - percentFromNumber h 1.0) - 32
-  topBarYpos = percentFromNumber h 1.0
-
-barCommand :: (Int, Int) -> String -> String
-barCommand res pos = printf command pos ++ generateBarPosition res pos
-  where command = "xmobar $XMONAD_CONFIG_DIR/xmobarrc/%s.hs -p "
-
-spawnXmobar :: X ()
-spawnXmobar = do
-  resolution <- liftIO getScreenSize
-  spawnNamedPipe (barCommand resolution "top") "xmobarTop"
-  spawnNamedPipe (barCommand resolution "bot") "xmobarBot"
 
 startupHook :: X ()
 startupHook = do
-  spawnXmobar
+  -- spawnXmobar
   -- docksStartupHook
   spawn "betterlockscreen -w"
   setWMName "xmonad"
