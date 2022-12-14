43aa6f449c19523ef3365060e11f284c65d8034b
Lenivaya <xocada@gmail.com>
2022-12-14 23:42:03 +0200

    add some functions for wrapping packages
    Former-commit-id: 2d216886b84bf5a8925f1bb4d2c4f012a9258426

---
+++ lib/packages.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+with builtins;
+with lib; rec {
+  optimizeWithFlags = pkg: flags:
+    pkgs.lib.overrideDerivation pkg (old: let
+      newflags = pkgs.lib.foldl' (acc: x: "${acc} ${x}") "" flags;
+      oldflags =
+        if (pkgs.lib.hasAttr "NIX_CFLAGS_COMPILE" old)
+        then "${old.NIX_CFLAGS_COMPILE}"
+        else "";
+    in {
+      NIX_CFLAGS_COMPILE = "${oldflags} ${newflags}";
+    });
+
+  optimizeForThisHost = pkg:
+    optimizeWithFlags pkg ["-O3" "-march=native" "-fPIC"];
+
+  withDebuggingCompiled = pkg:
+    optimizeWithFlags pkg ["-DDEBUG"];
+}
