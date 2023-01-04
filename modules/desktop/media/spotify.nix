43044df928dbf63925ef44828cb59e76ce89b24c
Lenivaya <xocada@gmail.com>
2023-01-04 18:30:36 +0200

    update spotify module

--- modules/desktop/media/spotify.nix
+++ modules/desktop/media/spotify.nix
@@ -11,6 +11,7 @@ with lib.my; let
   inherit (inputs) spicetify-nix;
 
   cfg = config.modules.desktop.media.spotify;
+  spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
 in {
   options.modules.desktop.media.spotify.enable = mkBoolOpt false;
 
@@ -19,40 +20,41 @@ in {
   config = mkIf cfg.enable {
     programs.spicetify = {
       enable = true;
+
       spotifyPackage = pkgs.unstable.spotify-unwrapped;
       spicetifyPackage = pkgs.spicetify-cli.overrideAttrs (oa: rec {
         pname = "spicetify-cli";
-        version = "2.14.3";
+        version = "2.15.0";
         src = pkgs.fetchgit {
           url = "https://github.com/spicetify/${pname}";
           rev = "v${version}";
-          sha256 = "sha256-7bCl8VfkMhoTBnr+O+oBYQeSV2sRwlP/qUkNkYerZdU=";
+          sha256 = "sha256-o1vzhD24chec4orMHoCJ8mHsEDhcX8zz3BJ6VzfT/NE=";
         };
       });
-      # theme = "SpotifyNoPremium";
-      theme = "Nord";
 
-      enabledCustomApps = with spicetify-nix.pkgs.apps; [
+      # theme = "SpotifyNoPremium";
+      theme = spicePkgs.themes.Nord;
+      # theme = spicePkgs.themes.SpotifyNoPremium;
+
+      enabledCustomApps = with spicePkgs.apps; [
         lyrics-plus
         localFiles
         marketplace
       ];
-      enabledExtensions = [
-        "powerBar.js"
-        "keyboardShortcut.js"
-        "seekSong.js"
-        "goToSong.js"
+      enabledExtensions = with spicePkgs.extensions; [
+        powerBar
+        keyboardShortcut
+        seekSong
+        goToSong
 
-        "fullAppDisplayMod.js" #"fullAppDisplay.js"
-        # "shuffle+.js"
-        # "hidePodcasts.js"
-        "fullAlbumDate.js"
-        "playlistIcons.js"
-        "wikify.js"
-        "adblock.js"
+        fullAppDisplayMod
+        fullAlbumDate
+        playlistIcons
+        wikify
+        # adblock
+        # charliesAdblock
 
-        "wikify.js"
-        "groupSession.js"
+        groupSession
       ];
     };
   };
