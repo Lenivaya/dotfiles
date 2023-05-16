22ef17a14dc2c98d5d7bef86305e3778965b687d
Lenivaya <xocada@gmail.com>
2023-05-16 21:47:00 +0300

    little changes to scripts

--- config/xmonad/scripts/toggle-compositor.sh
+++ config/xmonad/scripts/toggle-compositor.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-active=$(systemctl --user is-active picom)
-
-if ! [[ $active = "active" ]]; then
+if ! systemctl --user is-active --quiet picom; then
 	systemctl --user start picom &
 else
 	systemctl --user stop picom &
