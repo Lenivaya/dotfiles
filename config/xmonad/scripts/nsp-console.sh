57de0453bc2daf1008c6df41646893c383ba76cb
Lenivaya <xocada@gmail.com>
2023-05-16 21:47:00 +0300

    little changes to scripts
    Former-commit-id: 22ef17a14dc2c98d5d7bef86305e3778965b687d

--- config/xmonad/scripts/nsp-console.sh
+++ config/xmonad/scripts/nsp-console.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if tmux list-sessions | grep -q "scratchpad"; then
+session_name="scratchpad"
+
+if tmux has-session -t "$session_name"; then
 	tmux attach-session -t "scratchpad"
 else
-	tmux new-session -s "scratchpad" -n "scratchpad" -c "$HOME"
+	tmux new-session -s "$session_name" -n "$session_name" -c "$HOME"
 fi
