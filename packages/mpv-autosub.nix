a23fd989bbb26f46368db2bdccf6085a4f3b5c35
Lenivaya <xocada@gmail.com>
2023-06-15 21:41:14 +0300

    add mpv autosub
    Former-commit-id: e073eeb99b24ecae632f94175697b6e4c300e1fd

---
+++ packages/mpv-autosub.nix
@@ -0,0 +1,28 @@
+{
+  fetchFromGitHub,
+  python3Packages,
+  stdenvNoCC,
+}:
+stdenvNoCC.mkDerivation rec {
+  pname = "mpv-autosub";
+  version = "git";
+
+  src = fetchFromGitHub {
+    owner = "davidde";
+    repo = pname;
+    rev = "35115355bd339681f97d067538356c29e5b14afa";
+    hash = "sha256-BKT/Tzwl5ZA4fbdc/cxz0+CYc1zyY/KOXc58x5GYow0=";
+  };
+
+  postPatch = ''
+    substituteInPlace autosub.lua \
+      --replace '/home/david/.local/bin/subliminal' \
+      '${python3Packages.subliminal}/bin/subliminal'
+  '';
+
+  installPhase = ''
+    install -Dm644 autosub.lua $out/share/mpv/scripts/autosub.lua
+  '';
+
+  passthru.scriptName = "autosub.lua";
+}
