ed5b5773aef7f2f3d21961b7fa712ff4ad72995a
Lenivaya <xocada@gmail.com>
2023-06-21 12:23:19 +0300

    add progressbar module
    Former-commit-id: 9215ad1f30ef0212276a83512a6ed768f8226611

---
+++ modules/services/progressbar.nix
@@ -0,0 +1,53 @@
+# Service to have simple command that will spawn
+# graphic progessbars for volume, brightness etc.
+# So "progressbar 80" will spawn 80% bar
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.my; let
+  inherit (config.dotfiles) configDir;
+  cfg = config.modules.services.progressbar;
+
+  progressPipe = "/tmp/progressbarPipe";
+  progressScript = pkgs.writeShellApplication {
+    name = "progressbar";
+    text = ''
+      if [[ $1 =~ ^[+-]?[0-9]+(\.[0-9]+)?!?$ ]]; then
+        echo "$1" > "${progressPipe}"
+      fi
+    '';
+  };
+in {
+  options.modules.services.progressbar = {
+    enable = mkBoolOpt true;
+    styles = mkOpt types.lines "";
+  };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      environment.systemPackages = with pkgs; [progressScript];
+
+      systemd.user.services.progressbar = {
+        description = "X progress bar using xob";
+        wants = ["graphical-session.target"];
+        wantedBy = ["graphical-session.target"];
+        after = ["graphical-session.target"];
+
+        path = with pkgs; [xob];
+        script = ''
+          rm -f ${progressPipe} && mkfifo ${progressPipe}
+          tail -f ${progressPipe} | xob
+        '';
+      };
+    }
+
+    (mkIf (notEmptyString cfg.styles) {
+      home.configFile."xob/styles.cfg".text = cfg.styles;
+    })
+  ]);
+}
